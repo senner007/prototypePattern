597528b02bfaafdeac5210e07f877111f7a38ddd
senner007 <nielshtg@hotmail.com>
2017-06-21 14:17:29 +0200

    beautify again

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 function Person(gender, job) {
   this.gender = gender;
   this.occupation = job;
-
-
 }
+
 Person.prototype.sayHi = function() {
   alert('Hi, my name is ' + this.name + ', I am a ' + this.gender)
-
 }
 
 function FemaleSecretary(name) {
-
   Person.call(this, 'girl', 'secretary')
   this.name = name;
-
 }
 FemaleSecretary.prototype = Object.create(Person.prototype)
 
 var tanja = new FemaleSecretary('Tanja');
-
 console.log(tanja)
 tanja.sayHi()
