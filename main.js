3f631e6806a2e2e4e263be6cdfe1e73de63bd0b5
senner007 <nielshtg@hotmail.com>
2017-06-21 14:16:42 +0200

    beautify

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-function Person (gender, job) {
-	this.gender = gender;
+function Person(gender, job) {
+  this.gender = gender;
   this.occupation = job;
 
 
 }
-Person.prototype.sayHi = function () {
-	alert('Hi, my name is ' + this.name + ', I am a ' + this.gender)
+Person.prototype.sayHi = function() {
+  alert('Hi, my name is ' + this.name + ', I am a ' + this.gender)
 
 }
 
-function FemaleSecretary (name) {
+function FemaleSecretary(name) {
 
-	Person.call(this, 'girl', 'secretary')
+  Person.call(this, 'girl', 'secretary')
   this.name = name;
 
 }
